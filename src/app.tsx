7f023031b43646037207cf551e633f30a13daadc
vladenego <mistervlad3@gmail.com>
2021-02-21 19:38:29 +0200

    chore: create clean react, typescript, emotion setup with parcel

---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+import { styles } from "../styles";
+
+export const App = () => {
+  return (
+    <div css={styles.main}>
+      <p css={styles.title}>React clean setup</p>
+    </div>
+  );
+};
---
+++ styles.ts
@@ -0,0 +1,10 @@
+import { css } from "@emotion/core";
+
+export const styles = {
+  main: css``,
+
+  title: css`
+    font-size: 25;
+    text-align: center;
+  `,
+};
